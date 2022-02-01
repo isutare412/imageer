d278f8c4cfdafe52a3493e633192918c6c0c4038
Redshore <isutare412@gmail.com>
2022-02-01 16:44:59 +0900

    Manage transaction manually

--- api-server/pkg/core/user/service.go
+++ api-server/pkg/core/user/service.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"gorm.io/gorm"
-
 	"github.com/isutare412/imageer/api-server/pkg/core/auth"
 )
 
@@ -21,7 +19,7 @@ type service struct {
 	authSvc auth.Service
 }
 
-func (s *service) Create(ctx context.Context, user *User, password string) (int64, error) {
+func (s *service) Create(ctx context.Context, user *User, password string) (id int64, err error) {
 	user.Privilege = PrivilegeUser
 
 	hashed, err := s.authSvc.Hash(password)
@@ -30,31 +28,30 @@ func (s *service) Create(ctx context.Context, user *User, password string) (int6
 	}
 	user.Password = hashed
 
-	var newID int64
-	if err := s.repo.Session(ctx).Transaction(func(tx *gorm.DB) error {
-		if err := tx.Create(user).Error; err != nil {
-			return fmt.Errorf("on create user: %w", err)
+	tx := s.repo.Session(ctx).Begin()
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
 		}
+	}()
 
-		newID = user.ID
-		return nil
-	}); s.repo.IsErrDuplicate(err) {
-		return 0, ErrDuplicate
+	err = tx.Create(user).Error
+	if s.repo.IsErrDuplicate(err) {
+		return 0, fmt.Errorf("email[%s] duplicated: %w", user.Email, ErrDuplicate)
 	} else if err != nil {
 		return 0, fmt.Errorf("on create user: %w", err)
 	}
-
-	return newID, nil
+	return user.ID, nil
 }
 
 func (s *service) GetByEmailPassword(ctx context.Context, email, password string) (*User, error) {
+	db := s.repo.Session(ctx)
+
 	var user User
-	if err := s.repo.Session(ctx).Transaction(func(tx *gorm.DB) error {
-		if err := tx.Where("email = ?", email).First(&user).Error; err != nil {
-			return fmt.Errorf("on get user by email: %w", err)
-		}
-		return nil
-	}); s.repo.IsErrNotFound(err) {
+	err := db.Where("email = ?", email).First(&user).Error
+	if s.repo.IsErrNotFound(err) {
 		return nil, ErrUserNotFound
 	} else if err != nil {
 		return nil, fmt.Errorf("on get user by email: %w", err)
@@ -63,46 +60,48 @@ func (s *service) GetByEmailPassword(ctx context.Context, email, password string
 	if ok := s.authSvc.Compare(password, user.Password); !ok {
 		return nil, ErrPasswordNotCorrect
 	}
-
 	return &user, nil
 }
 
 func (s *service) GetByID(ctx context.Context, id int64) (*User, error) {
+	db := s.repo.Session(ctx)
+
 	var user User
-	if err := s.repo.Session(ctx).Transaction(func(tx *gorm.DB) error {
-		if err := tx.First(&user, id).Error; err != nil {
-			return fmt.Errorf("on get by id: %w", err)
-		}
-		return nil
-	}); s.repo.IsErrNotFound(err) {
+	err := db.First(&user, id).Error
+	if s.repo.IsErrNotFound(err) {
 		return nil, ErrUserNotFound
 	} else if err != nil {
 		return nil, fmt.Errorf("on get by id: %w", err)
 	}
-
 	return &user, nil
 }
 
-func (s *service) UpdateCredit(ctx context.Context, id int64, delta int64) (*User, error) {
-	var user User
-	if err := s.repo.Session(ctx).Transaction(func(tx *gorm.DB) error {
-		if err := tx.First(&user, id).Error; err != nil {
-			return fmt.Errorf("on update credit: %w", err)
+func (s *service) UpdateCredit(ctx context.Context, id int64, delta int64) (user *User, err error) {
+	tx := s.repo.Session(ctx).Begin()
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+		} else {
+			tx.Commit()
 		}
+	}()
 
-		newCredit := user.Credit + delta
-		if newCredit < 0 {
-			newCredit = 0
-		}
-		if err := tx.Model(&user).Update("credit", newCredit).Error; err != nil {
-			return fmt.Errorf("on update credit: %w", err)
-		}
-		user.Credit = newCredit
-		return nil
-	}); err != nil {
+	err = tx.First(user, id).Error
+	if err != nil {
+		return nil, ErrUserNotFound
+	}
+
+	newCredit := user.Credit + delta
+	if newCredit < 0 {
+		newCredit = 0
+	}
+	err = tx.Model(&user).Update("credit", newCredit).Error
+	if err != nil {
 		return nil, fmt.Errorf("on update credit: %w", err)
 	}
-	return &user, nil
+	user.Credit = newCredit
+
+	return user, nil
 }
 
 func NewService(repo Repo, authSvc auth.Service) Service {
