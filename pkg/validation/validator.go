b9fc3c39709438aadb1c014dce122f7bf57e1e48
Redshore <isutare412@gmail.com>
2025-08-29 00:34:25 +0900

    refactor: use single validator instance

--- pkg/validation/validator.go
+++ pkg/validation/validator.go
@@ -9,24 +9,28 @@ import (
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
 	trans_en "github.com/go-playground/validator/v10/translations/en"
+
+	"github.com/isutare412/imageer/pkg/apperr"
 )
 
-var globalTranslator ut.Translator
-
-func init() {
-	globalTranslator = ut.New(locale_us.New()).GetFallback()
+// Validator validates structs using tags.
+// Ref: https://pkg.go.dev/github.com/go-playground/validator/v10
+type Validator struct {
+	validate   *validator.Validate
+	translator ut.Translator
 }
 
-func NewValidate() *validator.Validate {
-	v := validator.New(validator.WithRequiredStructEnabled())
+func NewValidator() Validator {
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	translator := ut.New(locale_us.New()).GetFallback()
 
 	// Support english error translations
-	if err := trans_en.RegisterDefaultTranslations(v, globalTranslator); err != nil {
+	if err := trans_en.RegisterDefaultTranslations(validate, translator); err != nil {
 		panic(fmt.Errorf("registering translator: %w", err))
 	}
 
 	// Use json tag as field name if exists
-	v.RegisterTagNameFunc(func(fld reflect.StructField) string {
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
 		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
 		if name == "-" {
 			return ""
@@ -34,9 +38,25 @@ func NewValidate() *validator.Validate {
 		return name
 	})
 
-	return v
+	return Validator{
+		validate:   validate,
+		translator: translator,
+	}
 }
 
-func TranslateError(err validator.FieldError) string {
-	return err.Translate(globalTranslator)
+func (v Validator) Validate(i any) error {
+	err := v.validate.Struct(i)
+	if err == nil {
+		return nil
+	}
+
+	errs := err.(validator.ValidationErrors)
+	errorMsgs := make([]string, 0, len(errs))
+	for _, e := range errs {
+		errorMsgs = append(errorMsgs, e.Translate(v.translator))
+	}
+
+	return apperr.NewError(apperr.CodeBadRequest).
+		WithSummary("Validation failed").
+		WithDetail("%s", strings.Join(errorMsgs, "\n"))
 }
