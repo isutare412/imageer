621e4fea710e7dbe92a7848223c76a2cff1cb7c0
Redshore <isutare412@gmail.com>
2025-10-26 05:01:31 +0900

    chore: add more apperr code

--- pkg/apperr/code.go
+++ pkg/apperr/code.go
@@ -26,23 +26,29 @@ var (
 	// 405 Method Not Allowed
 	CodeMethodNotAllowed = Code{5000, "METHOD_NOT_ALLOWED"}
 
-	// 409 Conflict
-	CodeConflict = Code{6000, "CONFLICT"}
+	// 406 Not Acceptable
+	CodeNotAcceptable = Code{6000, "NOT_ACCEPTABLE"}
 
-	// 413 Entity Too Large
-	CodeEntityTooLarge = Code{7000, "ENTITY_TOO_LARGE"}
+	// 409 Conflict
+	CodeConflict = Code{7000, "CONFLICT"}
+
+	// 413 Request Entity Too Large
+	CodeRequestEntityTooLarge = Code{8000, "REQUEST_ENTITY_TOO_LARGE"}
 
 	// 422 Unprocessable Entity
-	CodeUnprocessableEntity = Code{8000, "UNPROCESSABLE_ENTITY"}
+	CodeUnprocessableEntity = Code{9000, "UNPROCESSABLE_ENTITY"}
+
+	// 429 Too Many Requests
+	CodeTooManyRequests = Code{10000, "TOO_MANY_REQUESTS"}
 
 	// 500 Internal Server Error
-	CodeInternalServerError = Code{9000, "INTERNAL_SERVER_ERROR"}
+	CodeInternalServerError = Code{11000, "INTERNAL_SERVER_ERROR"}
 
 	// 501 Not Implemented
-	CodeNotImplemented = Code{10000, "NOT_IMPLEMENTED"}
+	CodeNotImplemented = Code{12000, "NOT_IMPLEMENTED"}
 
 	// 503 Service Unavailable
-	CodeServiceUnavailable = Code{11000, "SERVICE_UNAVAILABLE"}
+	CodeServiceUnavailable = Code{13000, "SERVICE_UNAVAILABLE"}
 )
 
 func DefaultCode(statusCode int) Code {
@@ -57,12 +63,16 @@ func DefaultCode(statusCode int) Code {
 		return CodeNotFound
 	case http.StatusMethodNotAllowed:
 		return CodeMethodNotAllowed
+	case http.StatusNotAcceptable:
+		return CodeNotAcceptable
 	case http.StatusConflict:
 		return CodeConflict
 	case http.StatusRequestEntityTooLarge:
-		return CodeEntityTooLarge
+		return CodeRequestEntityTooLarge
 	case http.StatusUnprocessableEntity:
 		return CodeUnprocessableEntity
+	case http.StatusTooManyRequests:
+		return CodeTooManyRequests
 	case http.StatusInternalServerError:
 		return CodeInternalServerError
 	case http.StatusNotImplemented:
@@ -90,16 +100,20 @@ func (c Code) HTTPStatusCode() int {
 	case c.id >= 5000 && c.id < 6000:
 		return http.StatusMethodNotAllowed
 	case c.id >= 6000 && c.id < 7000:
-		return http.StatusConflict
+		return http.StatusNotAcceptable
 	case c.id >= 7000 && c.id < 8000:
-		return http.StatusRequestEntityTooLarge
+		return http.StatusConflict
 	case c.id >= 8000 && c.id < 9000:
-		return http.StatusUnprocessableEntity
+		return http.StatusRequestEntityTooLarge
 	case c.id >= 9000 && c.id < 10000:
-		return http.StatusInternalServerError
+		return http.StatusUnprocessableEntity
 	case c.id >= 10000 && c.id < 11000:
-		return http.StatusNotImplemented
+		return http.StatusTooManyRequests
 	case c.id >= 11000 && c.id < 12000:
+		return http.StatusInternalServerError
+	case c.id >= 12000 && c.id < 13000:
+		return http.StatusNotImplemented
+	case c.id >= 13000 && c.id < 14000:
 		return http.StatusServiceUnavailable
 	default:
 		return http.StatusInternalServerError
