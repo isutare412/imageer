3644c7d069a8487f47237ab0230aa405483c89d0
Redshore <isutare412@gmail.com>
2026-03-17 21:46:58 +0900

    chore: bump up otel semconv version

--- pkg/tracing/peer_service.go
+++ pkg/tracing/peer_service.go
@@ -1,12 +1,12 @@
 package tracing
 
-import semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+import semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
 
 var (
-	PeerServiceGoogleOIDC = semconv.PeerService("google-oidc")
-	PeerServicePostgres   = semconv.PeerService("postgres")
-	PeerServiceValkey     = semconv.PeerService("valkey")
-	PeerServiceAWSSQS     = semconv.PeerService("aws-sqs")
-	PeerServiceAWSS3      = semconv.PeerService("aws-s3")
-	PeerServiceInternet   = semconv.PeerService("internet")
+	PeerServiceGoogleOIDC = semconv.ServicePeerName("google-oidc")
+	PeerServicePostgres   = semconv.ServicePeerName("postgres")
+	PeerServiceValkey     = semconv.ServicePeerName("valkey")
+	PeerServiceAWSSQS     = semconv.ServicePeerName("aws-sqs")
+	PeerServiceAWSS3      = semconv.ServicePeerName("aws-s3")
+	PeerServiceInternet   = semconv.ServicePeerName("internet")
 )
